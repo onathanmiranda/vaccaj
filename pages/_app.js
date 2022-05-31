4aa232ebc942e66c11770c6f2ff096a7b2603359
nathan-miranda-soma <nathan.miranda@somagrupo.com.br>
2022-05-31 01:26:42 -0300

    Add Service Worker && Manifest

--- pages/_app.js
+++ pages/_app.js
@@ -28,10 +28,20 @@ function ContextsWrapper({ children }) {
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script id="serviceworker">{`
+            if ("serviceWorker" in navigator) {
+              navigator.serviceWorker.register("/service-worker.js");
+            }
+      `}</Script>
       {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
         <>
-          <Script id="cookies">
-            {`function getCookie(cname) {
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <Script id="gtm" strategy="lazyOnload">
+            {`
+              function getCookie(cname) {
                 let name = cname + "=";
                 let ca = document.cookie.split(';');
                 for(let i = 0; i < ca.length; i++) {
@@ -44,14 +54,8 @@ function MyApp({ Component, pageProps }) {
                   }
                 }
                 return "";
-              }`}
-          </Script>
-          <Script
-            strategy="lazyOnload"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <Script id="gtm" strategy="lazyOnload">
-            {`
+              }
+              
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
 
@@ -79,11 +83,89 @@ function MyApp({ Component, pageProps }) {
       )}
       <Head>
         <meta charset="UTF-8" />
-        <title>Vaccaj - Método Prático</title>
+        <title>Vaccaj - Método Prático de Canto</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://vaccaj.vercel.app/" />
+        <meta name="theme-color" content="#19171c" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
         <meta
           name="description"
-          content="Acesse as músicas do método Vaccaj de maneira simplificada."
+          content="Estude vocalizes e vaccajs de forma simplificada."
+        />
+        <link rel="shortcut icon" sizes="180x180" href="/images/icon-180.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/icon-180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/icon-32.png"
+        />
+        <link rel="manifest" href="/manifest.json"></link>
+
+        {/* <!-- iPad retina portrait startup image --> */}
+        <link
+          href="/assets/startup/apple-touch-startup-image-1536.png"
+          media="(device-width: 768px) and (device-height: 1024px)
+                     and (-webkit-device-pixel-ratio: 2)
+                     and (orientation: portrait)"
+          rel="apple-touch-startup-image"
+        />
+
+        {/* <!-- iPad non-retina portrait startup image --> */}
+        <link
+          href="/assets/startup/apple-touch-startup-image-768.png"
+          media="(device-width: 768px) and (device-height: 1024px)
+                        and (-webkit-device-pixel-ratio: 1)
+                        and (orientation: portrait)"
+          rel="apple-touch-startup-image"
+        />
+
+        {/* <!-- iPhone 6 Plus portrait startup image --> */}
+        <link
+          href="/assets/startup/apple-touch-startup-image-1242.png"
+          media="(device-width: 414px) and (device-height: 736px)
+                        and (-webkit-device-pixel-ratio: 3)
+                        and (orientation: portrait)"
+          rel="apple-touch-startup-image"
+        />
+
+        {/* <!-- iPhone 6 startup image --> */}
+        <link
+          href="/assets/startup/apple-touch-startup-image-750.png"
+          media="(device-width: 375px) and (device-height: 667px)
+                        and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+
+        {/* <!-- iPhone 5 startup image --> */}
+        <link
+          href="/assets/startup/apple-touch-startup-image-640.png"
+          media="(device-width: 320px) and (device-height: 568px)
+                        and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+
+        {/* <!-- iPhone < 5 retina startup image --> */}
+        <link
+          href="/assets/startup/apple-touch-startup-image-640.png"
+          media="(device-width: 320px) and (device-height: 480px)
+                        and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+
+        {/* <!-- iPhone < 5 non-retina startup image --> */}
+        <link
+          href="/assets/startup/apple-touch-startup-image-320.png"
+          media="(device-width: 320px) and (device-height: 480px)
+                        and (-webkit-device-pixel-ratio: 1)"
+          rel="apple-touch-startup-image"
         />
       </Head>
       <ContextsWrapper>
