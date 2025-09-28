6576946d394b2fd7cf7c7db83df9f53e935dd666
Nathan Miranda <nathandsmiranda@gmail.com>
2025-09-28 18:52:18 -0300

    Installs Clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 import Modulos from "@/models/Modulos";
 
@@ -41,6 +42,15 @@ export default function RootLayout({ children }) {
             </div>
           </div>
         </PlayerContextProvider>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
       </body>
       <GoogleAnalytics gaId="G-V8RDFC06E6" />
     </html>
